5b2020348043dbdbba9ed55aec8579bccfbda7c2
charles <limyifan1997@gmail.com>
2023-09-25 12:22:29 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Founder Base
+
+## Setup
+
+Make sure to install the dependencies:
+
+```bash
+# yarn
+yarn install
+
+# npm
+npm install
+```
+
+## Development Server
+
+Start the development server on http://localhost:3000
+
+```bash
+npm run dev
+```
+
+## Production
+
+Build the application for production:
+
+```bash
+npm run build
+```
+
+Locally preview production build:
+
+```bash
+npm run preview
+```
+
+Check out the [deployment documentation](https://nuxt.com/docs/getting-started/deployment) for more information.
+# FounderBase
---
+++ app.vue
@@ -0,0 +1,52 @@
+<template>
+  <Html>
+  <Body>
+  <NuxtLayout>
+    <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+    <NuxtPage />
+  </NuxtLayout>
+  </Body>
+  </Html>
+</template>
+<script setup>
+
+useHead({
+  title: "i13ventures",
+  meta: [
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
+      hid: "i13ventures",
+      name: "i13ventures",
+      content: "i13ventures",
+    },
+  ],
+  link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+});
+
+onMounted(() => {
+});
+</script>
+<style lang="scss">
+body {
+  margin: 0;
+}
+
+p {
+  margin: 0;
+}
+
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.25s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+
+a {
+  text-decoration: none;
+}
+</style>
